f87206187f04d7c4a040344e75bfc142eb7645d1
baseballlover723 <rosspa@rose-hulman.edu>
2015-10-01 14:20:52 -0400

    setup empty tests

---
+++ src/testing/CardTest.java
@@ -0,0 +1,19 @@
+package testing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CardTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void initCardTest() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/testing/MainTest.java
@@ -0,0 +1,29 @@
+package testing;
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class MainTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void PlaythroughTest1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void PlaythroughTest2() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void PlaythroughTest3() {
+		fail("Not yet implemented");
+	}
+
+}
