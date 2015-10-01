1c7b5c443d7d43c07fff1a418ae37fedb2536f6c
baseballlover723 <rosspa@rose-hulman.edu>
2015-10-01 16:36:07 -0400

    basic init set test

--- src/testing/SetTest.java
+++ src/testing/SetTest.java
@@ -5,6 +5,13 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import setGame.Card;
+import setGame.Set;
+import enums.Color;
+import enums.Number;
+import enums.Shading;
+import enums.Shape;
+
 public class SetTest {
 
 	@Before
@@ -12,8 +19,15 @@ public class SetTest {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void initSetTest() {
+		Card card1 = new Card(Color.RED, Shape.SQUIGGLE, Shading.SOLID, Number.TWO);
+		Card card2 = new Card(Color.RED, Shape.SQUIGGLE, Shading.SOLID, Number.TWO);
+		Card card3 = new Card(Color.RED, Shape.SQUIGGLE, Shading.SOLID, Number.TWO);
+
+		set = new Set(card1, card2, card3);
+		assertEquals(card1, set.getFirstCard());
+		assertEquals(card2, set.getSecondCard());
+		assertEquals(card3, set.getThirdCard());
 	}
 
 }
