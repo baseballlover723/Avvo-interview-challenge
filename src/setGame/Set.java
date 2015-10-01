98c78221bdf7274387b114554627c1331ddee230
Philip Ross <rosspa@rose-hulman.edu>
2015-10-01 17:11:04 -0400

    added valid attribute to set

--- src/setGame/Set.java
+++ src/setGame/Set.java
@@ -11,5 +11,21 @@ public class Set {
 		this.card2 = card2;
 		this.card3 = card3;
 	}
+	
+	public Card getCard1() {
+		return this.card1;
+	}
+ 
+	public Card getCard2() {
+		return this.card2;
+	}
+ 
+	public Card getCard3() {
+		return this.card3;
+	}
+	
+	public boolean isValid(){
+		return false;
+	}
  
 }
