c6e37c6c0bde67938175ce9eb3a2066159063872
Philip Ross <rosspa@rose-hulman.edu>
2015-10-01 17:01:39 -0400

    testing ssh from desktop

--- src/setGame/Set.java
+++ src/setGame/Set.java
@@ -1,4 +1,15 @@
 package setGame;
+
 public class Set {
 
+	private Card card1;
+	private Card card2;
+	private Card card3;
+
+	public Set(Card card1, Card card2, Card card3) {
+		this.card1 = card1;
+		this.card2 = card2;
+		this.card3 = card3;
+	}
+ 
 }
