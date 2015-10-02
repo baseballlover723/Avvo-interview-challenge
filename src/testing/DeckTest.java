3e84fcb3fa9036e6b8592f4c68b01ee432d90002
Philip Ross <rosspa@rose-hulman.edu>
2015-10-02 02:20:42 -0400

    added tests to test if there is a set revealed

--- src/testing/DeckTest.java
+++ src/testing/DeckTest.java
@@ -5,6 +5,11 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import enums.Color;
+import enums.Number;
+import enums.Shading;
+import enums.Shape;
+import setGame.Card;
 import setGame.Deck;
 
 public class DeckTest {
@@ -28,5 +33,27 @@ public class DeckTest {
             assertTrue(deck.getRemainingCards().size() >= Deck.MIN_DECK_SIZE &&
                     deck.getRemainingCards().size() <= Deck.MAX_DECK_SIZE);
         }
+    }
+
+    @Test
+    public void hasSetRevealedTest() {
+        Card card1 = new Card(Color.RED, Shape.SQUIGGLE, Shading.SOLID, Number.ONE);
+        Card card2 = new Card(Color.RED, Shape.SQUIGGLE, Shading.SOLID, Number.TWO);
+        Card card3 = new Card(Color.RED, Shape.SQUIGGLE, Shading.SOLID, Number.THREE);
+        Card card4 = new Card(Color.RED, Shape.SQUIGGLE, Shading.EMPTY, Number.ONE);
+        Card card5 = new Card(Color.RED, Shape.SQUIGGLE, Shading.STRIPED, Number.ONE);
+
+        Deck deck = new Deck(new Card[]{card1, card2, card3, card4, card5});
+        assertTrue(deck.hasSetRevealed());
+    }
+    @Test
+    public void hasNoSetRevealedTest() {
+        Card card1 = new Card(Color.RED, Shape.SQUIGGLE, Shading.SOLID, Number.ONE);
+        Card card3 = new Card(Color.RED, Shape.SQUIGGLE, Shading.SOLID, Number.THREE);
+        Card card4 = new Card(Color.RED, Shape.SQUIGGLE, Shading.EMPTY, Number.ONE);
+        Card card10 = new Card(Color.GREEN, Shape.DIAMOND, Shading.EMPTY, Number.TWO);
+
+        Deck deck = new Deck(new Card[]{card1, card3, card4, card10});
+        assertFalse(deck.hasSetRevealed());
     }
 }
