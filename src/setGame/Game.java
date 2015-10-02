52d4e14b602f3a6a6d6b214635156b570a529d0f
Philip Ross <rosspa@rose-hulman.edu>
2015-10-02 18:01:22 -0400

    implmeneted constuctors for game

--- src/setGame/Game.java
+++ src/setGame/Game.java
@@ -1,10 +1,23 @@
 package setGame;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class Game {
+    private Deck deck;
+    private Board board;
+
+    public Game() {
+        this(new Deck(), new Board());
+    }
+
+    public Game(Deck deck, Board board) {
+        this.deck = deck;
+        this.board = board;
+    }
+
+    public Deck getDeck() {
+        return this.deck;
+    }
     
-
-
+    public Board getBoard() {
+        return this.board;
+    }
 }
