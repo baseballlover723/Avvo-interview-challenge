05e521f6dc3f5bedfc15034b3374f095a562a916
Philip Ross <rosspa@rose-hulman.edu>
2015-10-02 01:13:56 -0400

    added test for generating random cards

--- src/testing/CardTest.java
+++ src/testing/CardTest.java
@@ -2,6 +2,8 @@ package testing;
 
 import static org.junit.Assert.*;
 
+import java.util.HashSet;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -13,17 +15,35 @@ import enums.Shape;
 
 public class CardTest {
 
-	@Before
-	public void setUp() throws Exception {
-	}
+    @Before
+    public void setUp() throws Exception {
+    }
 
-	@Test
-	public void initCardTest() {
-		Card card = new Card(Color.RED, Shape.SQUIGGLE, Shading.SOLID, Number.TWO);
-		assertEquals(Color.RED, card.getColor());
-		assertEquals(Shape.SQUIGGLE, card.getShape());
-		assertEquals(Shading.SOLID, card.getShading());
-		assertEquals(Number.TWO, card.getNumber());
-	
-	}
+    @Test
+    public void initCardTest() {
+        Card card = new Card(Color.RED, Shape.SQUIGGLE, Shading.SOLID, Number.TWO);
+        assertEquals(Color.RED, card.getColor());
+        assertEquals(Shape.SQUIGGLE, card.getShape());
+        assertEquals(Shading.SOLID, card.getShading());
+        assertEquals(Number.TWO, card.getNumber());
+    }
+
+    @Test
+    public void randomCardTest() {
+        boolean gotBadLuck = false;
+        for (int k = 0; k < 10; k++) {
+            HashSet<Card> cards = new HashSet<Card>();
+            for (int i = 0; i < 10; i++) {
+                cards.add(Card.generateRandomCard());
+            }
+            // probably unique
+            if (cards.size() < 9) {
+                if (gotBadLuck) {
+                    fail("not sufficiently random");
+                }
+                gotBadLuck = true;
+            }
+        }
+        // if hasn't failed by here, card is sufficently random
+    }
 }
