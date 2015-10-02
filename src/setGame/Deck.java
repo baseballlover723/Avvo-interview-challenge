9e6ea3529134d5728d788abdf33aaf360eeea0f9
Philip Ross <rosspa@rose-hulman.edu>
2015-10-02 01:47:15 -0400

    implemented deck construtors

--- src/setGame/Deck.java
+++ src/setGame/Deck.java
@@ -1,6 +1,7 @@
 package setGame;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Deck {
     // assume that a deck must have between 12 and 75 cards
@@ -9,9 +10,14 @@ public class Deck {
     private ArrayList<Card> remainingCards;
 
     public Deck() {
+        this(new Random().nextInt(MAX_DECK_SIZE - MIN_DECK_SIZE) + MIN_DECK_SIZE);
     }
 
     public Deck(int deckSize) {
+        this.remainingCards = new ArrayList<Card>();
+        for (int k=0;k<deckSize;k++) {
+            this.remainingCards.add(Card.generateRandomCard());
+        }
     }
 
     public ArrayList<Card> getRemainingCards() {
