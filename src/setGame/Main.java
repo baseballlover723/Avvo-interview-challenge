7830f90c7ea5014e1eb8fcb345e6848d54879bae
Philip Ross <rosspa@rose-hulman.edu>
2015-10-02 01:53:28 -0400

    added the exact methods asked for in the main file, they mostly just redirect to one of the objects methods though

--- src/setGame/Main.java
+++ src/setGame/Main.java
@@ -1,9 +1,22 @@
 package setGame;
 
-import enums.Color;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("running main");
+    }
+    
+    // basically a redirect
+    public static boolean method1(Card card1, Card card2, Card card3) {
+        return new Set(card1, card2, card3).isValid();
+    }
+    
+    public static boolean method2(Card[] cards) {
+        return false;
+    }
+    
+    public static ArrayList<Set> method3() {
+        return new ArrayList<Set>();
     }
 }
