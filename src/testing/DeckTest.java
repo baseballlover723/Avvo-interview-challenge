ffaf3071c47e9c28508377998cbae1e25716d3bf
Philip Ross <rosspa@rose-hulman.edu>
2015-10-02 01:05:47 -0400

    tests creating decks

--- src/testing/DeckTest.java
+++ src/testing/DeckTest.java
@@ -5,15 +5,28 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import setGame.Deck;
+
 public class DeckTest {
 
-	@Before
-	public void setUp() throws Exception {
-	}
+    @Before
+    public void setUp() throws Exception {
+    }
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void initDeckWithSizeTest() {
+        int deckSize = 34;
+        Deck deck = new Deck(deckSize);
+        assertEquals(deckSize, deck.getRemainingCards().size());
+    }
 
+    @Test
+    public void initDeckWithRandomSizeTest() {
+        int numberOfTests = 10_000;
+        for (int k = 0; k < numberOfTests; k++) {
+            Deck deck = new Deck();
+            assertTrue(deck.getRemainingCards().size() >= Deck.MIN_DECK_SIZE &&
+                    deck.getRemainingCards().size() <= Deck.MAX_DECK_SIZE);
+        }
+    }
 }
