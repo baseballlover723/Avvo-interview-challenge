b09d9f078b89eb8f4748d424fc32ec982500f8cc
Philip Ross <rosspa@rose-hulman.edu>
2015-10-02 17:39:09 -0400

    added tests for initializing games

--- src/testing/GameTest.java
+++ src/testing/GameTest.java
@@ -1,37 +1,80 @@
 package testing;
-import static org.junit.Assert.assertNotNull;
+
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.fail;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import enums.Color;
+import enums.Number;
+import enums.Shading;
+import enums.Shape;
+import setGame.Board;
+import setGame.Card;
+import setGame.Deck;
 import setGame.Game;
-
 
 public class GameTest {
 
-	@Before
-	public void setUp() throws Exception {
-	}
+    @Before
+    public void setUp() throws Exception {
+    }
 
-	@Test
-	public void initGameTest() {
-	    assertNotNull(new Game());
-	}
-	
-	@Test
-	public void PlaythroughTest1() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void initGameTest() {
+        Game game = new Game();
+        assertFalse(game.getDeck().empty());
+        assertTrue(game.getBoard().empty());
+    }
 
-	@Test
-	public void PlaythroughTest2() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void initGameWithDeckTest() {
+        Card card1 = new Card(Color.RED, Shape.SQUIGGLE, Shading.SOLID, Number.ONE);
+        Card card2 = new Card(Color.RED, Shape.SQUIGGLE, Shading.SOLID, Number.TWO);
+        Card card3 = new Card(Color.RED, Shape.SQUIGGLE, Shading.SOLID, Number.THREE);
+        Card card4 = new Card(Color.RED, Shape.SQUIGGLE, Shading.EMPTY, Number.ONE);
+        Card card5 = new Card(Color.RED, Shape.SQUIGGLE, Shading.STRIPED, Number.ONE);
+        Card card6 = new Card(Color.RED, Shape.DIAMOND, Shading.SOLID, Number.ONE);
+        Card card7 = new Card(Color.RED, Shape.OVAL, Shading.SOLID, Number.ONE);
 
-	@Test
-	public void PlaythroughTest3() {
-		fail("Not yet implemented");
-	}
+        Card[] cards = new Card[] { card1, card2, card3, card4, card5, card6, card7 };
+        
+        Game game = new Game(new Deck(cards), new Board());
+        assertArrayEquals((Object[]) cards, game.getBoard().toArray());
+        assertFalse(game.getDeck().empty());
+    }
+
+    @Test
+    public void initGameWithBoardTest() {
+        Card card1 = new Card(Color.RED, Shape.SQUIGGLE, Shading.SOLID, Number.ONE);
+        Card card2 = new Card(Color.RED, Shape.SQUIGGLE, Shading.SOLID, Number.TWO);
+        Card card3 = new Card(Color.RED, Shape.SQUIGGLE, Shading.SOLID, Number.THREE);
+        Card card4 = new Card(Color.RED, Shape.SQUIGGLE, Shading.EMPTY, Number.ONE);
+        Card card5 = new Card(Color.RED, Shape.SQUIGGLE, Shading.STRIPED, Number.ONE);
+        Card card6 = new Card(Color.RED, Shape.DIAMOND, Shading.SOLID, Number.ONE);
+        Card card7 = new Card(Color.RED, Shape.OVAL, Shading.SOLID, Number.ONE);
+
+        Card[] cards = new Card[] { card1, card2, card3, card4, card5, card6, card7 };
+        
+        Game game = new Game(new Deck(), new Board(cards));
+        assertArrayEquals((Object[]) cards, game.getBoard().toArray());
+        assertFalse(game.getDeck().empty());
+    }
+
+    @Test
+    public void PlaythroughTest1() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void PlaythroughTest2() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void PlaythroughTest3() {
+        fail("Not yet implemented");
+    }
 
 }
