fe468cdb30cc6e4ca4b79b59c8691aad72078f87
Philip Ross <rosspa@rose-hulman.edu>
2015-10-02 02:48:04 -0400

    added messages on the methods to clarify answer

--- src/setGame/Main.java
+++ src/setGame/Main.java
@@ -6,23 +6,29 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("running main");
     }
-    
+
     // basically a redirect
     public static boolean method1(Card card1, Card card2, Card card3) {
-        return new Set(card1, card2, card3).isValid();
+        boolean validSet = new Set(card1, card2, card3).isValid();
+        if (validSet) {
+            System.out.println("The given cards are a valid set");
+        } else {
+            System.out.println("The given cards are not a valid set");
+        }
+        return validSet;
     }
-    
+
     public static boolean method2(Card[] cards) {
         Deck deck = new Deck(cards);
         boolean hasSetRevealed = deck.hasSetRevealed();
         if (hasSetRevealed) {
             System.out.println("Found a set on the given board");
-        }else {
+        } else {
             System.out.println("Could not find a set on the given board");
         }
         return hasSetRevealed;
     }
-    
+
     public static ArrayList<Set> method3() {
         return new ArrayList<Set>();
     }
