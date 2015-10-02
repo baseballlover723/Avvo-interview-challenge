d5aad89384940ae6e84a98605c5edb0137edad61
Philip Ross <rosspa@rose-hulman.edu>
2015-10-02 00:27:34 -0400

    sets now internally use an array of cards

--- src/setGame/Set.java
+++ src/setGame/Set.java
@@ -1,27 +1,29 @@
 package setGame;
 
-public class Set {
+import java.util.HashSet;
 
-	private Card card1;
-	private Card card2;
-	private Card card3;
+import enums.Color;
+
+public class Set {
+	private Card[] cards;
 
 	public Set(Card card1, Card card2, Card card3) {
-		this.card1 = card1;
-		this.card2 = card2;
-		this.card3 = card3;
+		this.cards = new Card[3];
+		this.cards[0] = card1;
+		this.cards[1] = card2;
+		this.cards[2] = card3;
 	}
 	
 	public Card getCard1() {
-		return this.card1;
+		return this.cards[0];
 	}
  
 	public Card getCard2() {
-		return this.card2;
+		return this.cards[1];
 	}
  
 	public Card getCard3() {
-		return this.card3;
+		return this.cards[2];
 	}
 	
 	public boolean isValid(){
