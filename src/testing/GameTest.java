b400b457bee14b3921e8238678ec6a741513e172
Philip Ross <rosspa@rose-hulman.edu>
2015-10-05 14:47:27 -0400

    added some shuffing in the 3rd playtthrough tests

--- src/testing/GameTest.java
+++ src/testing/GameTest.java
@@ -161,6 +161,8 @@ public class GameTest {
 
         Card[] cards = new Card[] { card1, card2, card3, card4, card5, card6, card7, card8, card9, card10, card11,
                 card12, card13, card14, card15, card16, card17, card18, card19, card20 };
+        Collections.shuffle(Arrays.asList(cards));
+
         Game game = new Game(new Deck(cards), new Board());
         game.startGame();
         game.playGame();
