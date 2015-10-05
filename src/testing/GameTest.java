cbecaf343867534ea4aa42d7af0b681d29595ec5
Philip Ross <rosspa@rose-hulman.edu>
2015-10-05 14:38:03 -0400

    added playthrough tests

--- src/testing/GameTest.java
+++ src/testing/GameTest.java
@@ -6,9 +6,9 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -71,7 +71,7 @@ public class GameTest {
     }
 
     @Test
-    public void PlaythroughTest1() {
+    public void playthroughTest1() {
         Card[] cards = new Card[12];
         for (int k = 0; k < 12; k++) {
             cards[k] = new Card(Color.RED, Shape.SQUIGGLE, Shading.SOLID, Number.ONE);
@@ -87,13 +87,14 @@ public class GameTest {
         game.playGame();
         ArrayList<Set> sets = game.getFoundSets();
 
-        assertTrue(equalLists(expectedSets,sets));
-
-        fail("Not yet implemented");
+        assertTrue(equalLists(expectedSets, sets));
     }
 
     @Test
-    public void PlaythroughTest2() {
+    public void playthroughTest2() {
+        ArrayList<Set> expectedSets = new ArrayList<Set>();
+        Card card0 = new Card(Color.RED, Shape.DIAMOND, Shading.SOLID, Number.ONE);
+        
         Card card1 = new Card(Color.RED, Shape.SQUIGGLE, Shading.SOLID, Number.ONE);
         Card card2 = new Card(Color.RED, Shape.SQUIGGLE, Shading.SOLID, Number.ONE);
         Card card3 = new Card(Color.RED, Shape.SQUIGGLE, Shading.SOLID, Number.ONE);
@@ -110,24 +111,77 @@ public class GameTest {
         Card card11 = new Card(Color.RED, Shape.SQUIGGLE, Shading.EMPTY, Number.ONE);
         Card card12 = new Card(Color.RED, Shape.SQUIGGLE, Shading.EMPTY, Number.ONE);
 
-        Card[] cards = new Card[] { card1, card2, card3, card4, card5, card6, card7, card8, card9, card10, card11,
-                card12 };
-        fail("Not yet implemented");
+        Card card13 = new Card(Color.RED, Shape.DIAMOND, Shading.EMPTY, Number.TWO);
+        Card card14 = new Card(Color.RED, Shape.DIAMOND, Shading.EMPTY, Number.TWO);
+        Card card15 = new Card(Color.RED, Shape.DIAMOND, Shading.EMPTY, Number.TWO);
+
+        Card card16 = new Card(Color.GREEN, Shape.OVAL, Shading.SOLID, Number.ONE);
+        Card card17 = new Card(Color.GREEN, Shape.OVAL, Shading.SOLID, Number.ONE);
+        Card card18 = new Card(Color.GREEN, Shape.OVAL, Shading.SOLID, Number.ONE);
+        
+        Card card19 = new Card(Color.GREEN, Shape.SQUIGGLE, Shading.EMPTY, Number.ONE);
+        Card card20 = new Card(Color.GREEN, Shape.SQUIGGLE, Shading.EMPTY, Number.ONE);
+
+        expectedSets.add(new Set(card1, card2, card3));
+        expectedSets.add(new Set(card4, card5, card6));
+        expectedSets.add(new Set(card7, card8, card9));
+        expectedSets.add(new Set(card10, card11, card12));
+        expectedSets.add(new Set(card13, card14, card15));
+        expectedSets.add(new Set(card16, card17, card18));
+
+        Card[] cards = new Card[] { card0, card1, card2, card3, card4, card5, card6, card7, card8, card9, card10,
+                card11, card12, card13, card14, card15, card16, card17, card18, card19, card20 };
+
+        Collections.shuffle(Arrays.asList(cards));
+        Game game = new Game(new Deck(cards), new Board());
+        game.startGame();
+        game.playGame();
+        ArrayList<Set> sets = game.getFoundSets();
+
+        assertTrue(equalLists(expectedSets, sets));
     }
 
     @Test
-    public void PlaythroughTest3() {
-        fail("Not yet implemented");
+    public void playthroughTest3() {
+        Card card1 = new Card(Color.RED, Shape.SQUIGGLE, Shading.SOLID, Number.ONE);
+        Card card2 = new Card(Color.RED, Shape.SQUIGGLE, Shading.SOLID, Number.TWO);
+        Card card3 = new Card(Color.RED, Shape.DIAMOND, Shading.SOLID, Number.THREE);
+        Card card4 = new Card(Color.GREEN, Shape.DIAMOND, Shading.EMPTY, Number.THREE);
+        Card card5 = new Card(Color.GREEN, Shape.DIAMOND, Shading.STRIPED, Number.THREE);
+        Card card6 = new Card(Color.GREEN, Shape.SQUIGGLE, Shading.EMPTY, Number.ONE);
+        Card card7 = new Card(Color.GREEN, Shape.SQUIGGLE, Shading.EMPTY, Number.TWO);
+        Card card8 = new Card(Color.PURPLE, Shape.DIAMOND, Shading.SOLID, Number.THREE);
+        Card card9 = new Card(Color.GREEN, Shape.DIAMOND, Shading.SOLID, Number.TWO);
+        Card card10 = new Card(Color.GREEN, Shape.DIAMOND, Shading.SOLID, Number.ONE);
+        Card card11 = new Card(Color.GREEN, Shape.SQUIGGLE, Shading.SOLID, Number.ONE);
+        Card card12 = new Card(Color.GREEN, Shape.SQUIGGLE, Shading.SOLID, Number.TWO);
+        Card card13 = new Card(Color.PURPLE, Shape.OVAL, Shading.EMPTY, Number.THREE);
+        Card card14 = new Card(Color.RED, Shape.OVAL, Shading.EMPTY, Number.TWO);
+        Card card15 = new Card(Color.RED, Shape.OVAL, Shading.EMPTY, Number.ONE);
+        Card card16 = new Card(Color.RED, Shape.SQUIGGLE, Shading.EMPTY, Number.ONE);
+        Card card17 = new Card(Color.RED, Shape.SQUIGGLE, Shading.EMPTY, Number.TWO);
+        Card card18 = new Card(Color.RED, Shape.OVAL, Shading.SOLID, Number.THREE);
+        Card card19 = new Card(Color.GREEN, Shape.OVAL, Shading.SOLID, Number.THREE);
+        Card card20 = new Card(Color.RED, Shape.OVAL, Shading.STRIPED, Number.THREE);
+
+        Card[] cards = new Card[] { card1, card2, card3, card4, card5, card6, card7, card8, card9, card10, card11,
+                card12, card13, card14, card15, card16, card17, card18, card19, card20 };
+        Game game = new Game(new Deck(cards), new Board());
+        game.startGame();
+        game.playGame();
+        
+        assertTrue(game.getFoundSets().isEmpty());
     }
 
-    // from the internet 
+    // from the internet
     // http://stackoverflow.com/questions/13501142/java-arraylist-how-can-i-tell-if-two-lists-are-equal-order-not-mattering
     public <T> boolean equalLists(ArrayList<T> expectedSets, ArrayList<T> sets) {
         if (expectedSets == null && sets == null) {
             return true;
         }
 
-        if ((expectedSets == null && sets != null) || expectedSets != null && sets == null || expectedSets.size() != sets.size()) {
+        if ((expectedSets == null && sets != null) || expectedSets != null && sets == null ||
+                expectedSets.size() != sets.size()) {
             return false;
         }
 
